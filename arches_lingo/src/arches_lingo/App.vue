8d513ca8871ade7efcdd149ed77ca483a961bff5
Jacob Walls <jwalls@fargeo.com>
2024-08-02 17:54:52 -0400

    Implement login, logout, name display

--- arches_lingo/src/arches_lingo/App.vue
+++ arches_lingo/src/arches_lingo/App.vue
@@ -1,7 +1,20 @@
-<script setup>
-import Foo from "@/arches_lingo/components/FooComponent.vue";
+<script setup lang="ts">
+import { ref } from "vue";
+
+import Toast from "primevue/toast";
+
+import HomePage from "@/arches_lingo/components/HomePage.vue";
+import LoginPage from "@/arches_lingo/components/LoginPage.vue";
+
+import type { User } from "@/arches_lingo/types";
+
+const user = ref<User | null>(null);
 </script>
 
 <template>
-    <Foo />
+    <component
+        :is="user ? HomePage : LoginPage"
+        v-model="user"
+    />
+    <Toast />
 </template>
