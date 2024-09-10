cb3ba0b9033e66a8f5ebebeda65c1cf477dd6203
Jacob Walls <jwalls@fargeo.com>
2024-09-10 15:08:52 -0400

    Consume more tokens from arches theme
    Fix header skip #87

--- arches_lingo/src/arches_lingo/constants.ts
+++ arches_lingo/src/arches_lingo/constants.ts
@@ -4,6 +4,7 @@ import type { UserRefAndSetter } from "@/arches_lingo/types.ts";
 
 export const ANONYMOUS = "anonymous";
 export const ERROR = "error";
+export const SECONDARY = "secondary";
 export const DEFAULT_ERROR_TOAST_LIFE = 8000;
 
 export const USER_KEY = Symbol() as InjectionKey<UserRefAndSetter>;
