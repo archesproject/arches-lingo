9f78cf1c5a626466246762ae39d95b55e1858493
Jacob Walls <jwalls@fargeo.com>
2024-10-21 19:05:26 -0400

    Reraise validation errors

--- arches_lingo/views/api/pythonic_models.py
+++ arches_lingo/views/api/pythonic_models.py
@@ -1,5 +1,7 @@
 from functools import partial
 
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
@@ -25,6 +27,27 @@ class PythonicModelAPIMixin:
     def update(self, request, *args, **kwargs):
         self.get_object = partial(self.get_object, user=request.user)
         return super().update(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        """Re-raise ValidationError as DRF ValidationError.
+
+        In 3.0 (2014), DRF decided to stop full_clean()'ing before save(),
+        which divorces DRF validation needs from model logic needing to
+        support the Django admin or similar ModelFormish patterns.
+        The stated reasons were:
+            - to avoid needing to know about big & scary full_clean(). Fine.
+            - to force expressing validation logic outside of models.
+        but to adhere to *that* here would require some way of generically
+        validating incoming tile data without knowledge of the resource/graph,
+        which isn't practical under this implementation.
+
+        Discussion:
+        https://github.com/encode/django-rest-framework/discussions/7850
+        """
+        try:
+            serializer.save()
+        except DjangoValidationError as django_error:
+            raise ValidationError(detail=django_error) from django_error
 
 
 class SchemeDetailView(PythonicModelAPIMixin, RetrieveUpdateDestroyAPIView):
