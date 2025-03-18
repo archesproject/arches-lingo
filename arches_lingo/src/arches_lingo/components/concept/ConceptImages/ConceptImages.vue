6e5e615d2d227a8b0191c63dc300bee79f83f993
Aaron Gundel <aaron.gundel@gmail.com>
2025-03-18 10:05:05 -0600

    initial commit of concept image viewer

---
+++ arches_lingo/src/arches_lingo/components/concept/ConceptImages/ConceptImages.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+import ProgressSpinner from "primevue/progressspinner";
+
+//import ConceptImagesEditor from "@/arches_lingo/components/concept/ConceptImages/components/ConceptImagesEditor.vue";
+import ConceptImagesViewer from "@/arches_lingo/components/concept/ConceptImages/components/ConceptImagesViewer.vue";
+
+import { EDIT, VIEW } from "@/arches_lingo/constants.ts";
+
+import { fetchLingoResourcePartial } from "@/arches_lingo/api.ts";
+
+import type { ConceptImages, DataComponentMode } from "@/arches_lingo/types.ts";
+
+const props = defineProps<{
+    mode: DataComponentMode;
+    sectionTitle: string;
+    componentName: string;
+    graphSlug: string;
+    nodegroupAlias: string;
+    resourceInstanceId: string | undefined;
+    tileId?: string;
+}>();
+
+const isLoading = ref(true);
+const tileData = ref<ConceptImages>();
+
+const shouldCreateNewTile = Boolean(props.mode === EDIT && !props.tileId);
+
+onMounted(async () => {
+    if (
+        props.resourceInstanceId &&
+        (props.mode === VIEW || !shouldCreateNewTile)
+    ) {
+        const sectionValue = await getSectionValue();
+        tileData.value = sectionValue[props.nodegroupAlias];
+    }
+
+    isLoading.value = false;
+});
+
+async function getSectionValue() {
+    try {
+        return await fetchLingoResourcePartial(
+            props.graphSlug,
+            props.resourceInstanceId as string,
+            props.nodegroupAlias,
+        );
+    } catch (error) {
+        console.error(error);
+    }
+}
+</script>
+
+<template>
+    <ProgressSpinner
+        v-if="isLoading"
+        style="width: 100%"
+    />
+
+    <template v-else>
+        <ConceptImagesViewer
+            v-if="mode === VIEW"
+            :tile-data="tileData"
+            :graph-slug="props.graphSlug"
+            :component-name="props.componentName"
+            :section-title="props.sectionTitle"
+            :nodegroup-alias="props.nodegroupAlias"
+        />
+        <!-- <ConceptImagesEditor
+            v-else-if="mode === EDIT"
+            :tile-data="tileData?.find((tileDatum) => tileDatum.tileid === props.tileId)
+                "
+            :component-name="props.componentName"
+            :section-title="props.sectionTitle"
+            :graph-slug="props.graphSlug"
+            :nodegroup-alias="props.nodegroupAlias"
+            :resource-instance-id="props.resourceInstanceId"
+            :tile-id="props.tileId"
+        /> -->
+    </template>
+</template>
---
+++ arches_lingo/src/arches_lingo/components/concept/ConceptImages/components/ConceptImagesViewer.vue
@@ -0,0 +1,169 @@
+<script setup lang="ts">
+import { inject, ref, onMounted } from "vue";
+import { useGettext } from "vue3-gettext";
+
+import Button from "primevue/button";
+
+import FileListWidget from "@/arches_component_lab/widgets/FileListWidget/FileListWidget.vue";
+
+import { DANGER, SECONDARY, VIEW } from "@/arches_lingo/constants.ts";
+import { useConfirm } from "primevue/useconfirm";
+
+import type {
+    ConceptImages,
+    DigitalObjectInstance,
+} from "@/arches_lingo/types.ts";
+import { fetchLingoResource } from "@/arches_lingo/api.ts";
+
+const props = defineProps<{
+    tileData: ConceptImages | undefined;
+    componentName: string;
+    sectionTitle: string;
+    graphSlug: string;
+    nodegroupAlias: string;
+}>();
+
+const resources = ref<DigitalObjectInstance[]>();
+const { $gettext } = useGettext();
+const confirm = useConfirm();
+
+onMounted(async () => {
+    if (props.tileData) {
+        resources.value = await Promise.all(
+            props.tileData.depicting_digital_asset_internal.map(
+                async (x) => await getConceptImageResource(x.resourceId),
+            ),
+        );
+    }
+});
+function confirmDelete() {
+    confirm.require({
+        header: $gettext("Confirmation"),
+        message: $gettext("Do you want to delete this concept image?"),
+        accept: () => {
+            console.log("do delete");
+        },
+        rejectProps: {
+            label: $gettext("Cancel"),
+            severity: SECONDARY,
+            outlined: true,
+        },
+        acceptProps: {
+            label: $gettext("Delete"),
+            severity: DANGER,
+        },
+    });
+}
+
+const openEditor = inject<(componentName: string) => void>("openEditor");
+async function getConceptImageResource(resourceInstanceId: string) {
+    try {
+        return await fetchLingoResource(
+            "digital_object_rdm_system",
+            resourceInstanceId as string,
+        );
+    } catch (error) {
+        console.error(error);
+    }
+}
+</script>
+
+<template>
+    <div class="section-header">
+        <h2>{{ props.sectionTitle }}</h2>
+        <Button
+            :label="$gettext('Add New Concept Image')"
+            @click="openEditor!(props.componentName)"
+        ></Button>
+    </div>
+    <div class="conceptImages">
+        <div
+            v-for="resource in resources"
+            :key="resource.resourceinstanceid"
+            class="conceptImage"
+        >
+            <div class="header">
+                <label
+                    for="conceptImage"
+                    class="element"
+                    >bobbobbobbob bobbobbobbob bob. bobbobbob bobbobbobbobbob
+                    bobbobbobbob bob. bobbobbob bobbobbobbobbob bobbobbobbob
+                    bob. bobbobbob bob bobbobbobbob bobbobbobbob bob. bobbobbob
+                    bob</label
+                >
+                <div class="element">
+                    <Button
+                        icon="pi pi-file-edit"
+                        @click="openEditor!(props.componentName)"
+                    />
+                    <Button
+                        icon="pi pi-trash"
+                        :aria-label="$gettext('delete')"
+                        severity="danger"
+                        outlined
+                        @click="confirmDelete()"
+                    />
+                </div>
+            </div>
+            <FileListWidget
+                node-alias="content"
+                graph-slug="digital_object_rdm_system"
+                :initial-value="resource.content.content"
+                :mode="VIEW"
+                class="conceptImage"
+            />
+            <div class="footer">
+                bobbobbobbob bobbobbobbob bob. bobbobbob bobbobbobbobbob
+                bobbobbobbob bob. bobbobbob bobbobbobbobbob bobbobbobbob bob.
+                bobbobbob bob bobbobbobbob bobbobbobbob bob. bobbobbob bob
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.section-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 0.125rem solid var(--p-menubar-border-color);
+}
+
+.conceptImages {
+    display: flex;
+    flex-direction: row;
+    align-items: start;
+}
+
+.conceptImage {
+    display: grid;
+    grid-template-rows: auto auto auto;
+    margin: 2rem 5rem;
+}
+
+.conceptImage .header {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    padding: 1rem 0;
+}
+
+.conceptImage .footer {
+    padding: 1rem 0;
+}
+
+.conceptImage .header .element {
+    display: flex;
+    align-items: center;
+}
+
+.conceptImage .header .element button {
+    margin: 0 0.5rem;
+}
+
+.conceptImages :deep(.mainImage) {
+}
+
+.conceptImages :deep(.p-galleria) {
+    border: none;
+}
+</style>
