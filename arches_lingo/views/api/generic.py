c26f31ba53e4f34c93960a7df7c6b5014a8bf12a
Jacob Walls <jwalls@fargeo.com>
2025-04-16 23:03:06 -0400

    Declare parser classes

--- arches_lingo/views/api/generic.py
+++ arches_lingo/views/api/generic.py
@@ -2,7 +2,9 @@ from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
+from rest_framework.parsers import JSONParser
 
+from arches_querysets.rest_framework.multipart_json_parser import MultiPartJSONParser
 from arches_querysets.rest_framework.permissions import RDMAdministrator
 from arches_querysets.rest_framework.serializers import ArchesResourceSerializer
 from arches_querysets.rest_framework.view_mixins import ArchesModelAPIMixin
@@ -14,18 +16,22 @@ class LingoResourceListCreateView(ArchesModelAPIMixin, ListCreateAPIView):
     permission_classes = [RDMAdministrator]
     serializer_class = ArchesResourceSerializer
     pagination_class = None
+    parser_classes = [JSONParser, MultiPartJSONParser]
 
 
 class LingoResourceDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
     permission_classes = [RDMAdministrator]
     serializer_class = ArchesResourceSerializer
+    parser_classes = [JSONParser, MultiPartJSONParser]
 
 
 class LingoTileListCreateView(ArchesModelAPIMixin, ListCreateAPIView):
     permission_classes = [RDMAdministrator]
     serializer_class = LingoTileSerializer
+    parser_classes = [JSONParser, MultiPartJSONParser]
 
 
 class LingoTileDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
     permission_classes = [RDMAdministrator]
     serializer_class = LingoTileSerializer
+    parser_classes = [JSONParser, MultiPartJSONParser]
