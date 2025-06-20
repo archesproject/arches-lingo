db9be16c09d1d81a19d55e0e026dcc2aa78c50b7
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-06-20 14:14:55 -0400

    Rough out updated sidenav w/ expand-collapse #342

--- arches_lingo/src/arches_lingo/components/sidenav/SideNav.vue
+++ arches_lingo/src/arches_lingo/components/sidenav/SideNav.vue
@@ -4,50 +4,139 @@ import { useGettext } from "vue3-gettext";
 
 import { routeNames } from "@/arches_lingo/routes.ts";
 
+import Button from "primevue/button";
+import PanelMenu from "primevue/panelmenu";
+
+import type { MenuItem } from "primevue/menuitem";
+
 const { $gettext } = useGettext();
 
-const items = ref([
+const expandedKeys = ref<Record<string, boolean>>({});
+const isExpanded = ref(false);
+const items = ref<MenuItem[]>([
     {
-        icon: "fa fa-home",
-        routeName: routeNames.root,
-        linkName: $gettext("Home"),
-    },
+        key: "navigation",
+        label: $gettext("Navigation"),
+        items: [
+            {
+                key: "home",
+                label: $gettext("Home"),
+                icon: "fa fa-home",
+                route: { name: routeNames.root },
+            } as MenuItem,
+            {
+                key: "advanced_search",
+                label: $gettext("Advanced Search"),
+                icon: "pi pi-search",
+                route: { name: routeNames.advancedSearch },
+            } as MenuItem,
+        ],
+    } as MenuItem,
+    {
+        key: "schemes",
+        label: $gettext("Schemes"),
+        icon: "pi pi-lightbulb",
+        route: { name: routeNames.schemes },
+    } as MenuItem,
 ]);
+
+function toggleAll() {
+    if (Object.keys(expandedKeys.value).length) collapseAll();
+    else expandAll();
+}
+
+function toggleNode(node: MenuItem) {
+    if (node.key && expandedKeys.value[node.key]) {
+        collapseNode(node);
+    } else {
+        expandNode(node);
+    }
+}
+
+function expandAll() {
+    isExpanded.value = true;
+    for (let node of items.value) {
+        expandNode(node);
+    }
+    expandedKeys.value = { ...expandedKeys.value };
+}
+
+function collapseAll() {
+    isExpanded.value = false;
+    expandedKeys.value = {};
+}
+
+function expandNode(node: MenuItem) {
+    if (node.key && node.items && node.items.length) {
+        expandedKeys.value[node.key] = true;
+
+        for (let child of node.items) {
+            expandNode(child);
+        }
+    }
+}
+
+function collapseNode(node: MenuItem) {
+    if (node.key && node.items && node.items.length) {
+        expandedKeys.value[node.key] = false;
+
+        for (let child of node.items) {
+            collapseNode(child);
+        }
+    }
+}
 </script>
 
 <template>
     <aside class="sidenav">
-        <div
-            v-for="item in items"
-            :key="item.routeName"
+        <Button
+            class="sidenav-header"
+            @click="toggleAll"
         >
-            <RouterLink
-                v-tooltip="{
-                    value: item.linkName,
-                    pt: { text: { style: { fontFamily: 'sans-serif' } } },
-                }"
-                :to="{ name: item.routeName }"
-                class="p-button p-component p-button-primary"
-                style="text-decoration: none"
-            >
-                <i
-                    :class="item.icon"
-                    aria-hidden="true"
-                ></i>
-            </RouterLink>
-        </div>
+            <i class="pi pi-bars toggle-icon"></i>
+            <!-- <span v-show="isExpanded" class="header-title">Navigation</span> -->
+        </Button>
+        <PanelMenu
+            :model="items"
+            :expanded-keys="expandedKeys"
+        >
+            <template #item="{ item }">
+                <router-link
+                    v-if="item.route"
+                    v-slot="{ href, navigate }"
+                    :to="item.route"
+                    custom
+                >
+                    <a
+                        :href="href"
+                        @click="navigate"
+                    >
+                        <span :class="item.icon"></span>
+                        <span v-if="isExpanded">{{ item.label }}</span>
+                    </a>
+                </router-link>
+                <a
+                    v-else
+                    @click="toggleNode(item)"
+                >
+                    <span :class="item.icon"></span>
+                    <span v-if="isExpanded">{{ item.label }}</span>
+                    <span
+                        v-if="item.items"
+                        class="pi pi-angle-down text-primary ml-auto"
+                    />
+                </a>
+            </template>
+        </PanelMenu>
     </aside>
 </template>
 
 <style scoped>
 .sidenav {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     border-right: 1px solid var(--p-menubar-border-color);
 }
 
-.p-button {
+.lingo-link {
     min-height: var(--p-button-lg-icon-only-width);
     min-width: var(--p-button-lg-icon-only-width);
     border-radius: 0;
