464d46e66dcb451ede38f9d090c6cbb64ac2c651
Jacob Walls <jwalls@fargeo.com>
2025-07-07 19:01:15 -0400

    Update validation pattern in custom serializer

--- arches_lingo/serializers.py
+++ arches_lingo/serializers.py
@@ -4,30 +4,29 @@ from rest_framework.exceptions import ValidationError
 
 from arches_controlled_lists.datatypes.datatypes import Reference, ReferenceDataType
 from arches_controlled_lists.models import ListItem
+from arches_querysets.models import ResourceTileTree
 from arches_querysets.rest_framework.serializers import ArchesTileSerializer
 
 
 class LingoTileSerializer(ArchesTileSerializer):
-
-    def validate_appellative_status(self, data):
+    def validate_appellative_status(self, data, initial_tile_data):
         if data:
             new_label_lang = None
             new_label_type = None
             if new_label_languages := self.get_reference_object(
-                data.get("appellative_status_ascribed_name_language")
+                data.appellative_status_ascribed_name_language
             ):
                 new_label_lang = new_label_languages[0]
             if new_label_types := self.get_reference_object(
-                data.get("appellative_status_ascribed_relation")
+                data.appellative_status_ascribed_relation
             ):
                 new_label_type = new_label_types[0]
 
             if new_label_lang and new_label_type:
-                # Still working on the right API for this.
-                # Don't want to do it too eagerly.
-                self.instance._enrich(self.graph_slug)
+                resource_pk = self.root.initial_data["resourceinstance"]
+                scheme = ResourceTileTree.get_tiles(self.graph_slug).get(pk=resource_pk)
                 self._check_pref_label_uniqueness(
-                    data, self.instance.resourceinstance, new_label_lang, new_label_type
+                    initial_tile_data, scheme, new_label_lang, new_label_type
                 )
 
         return data
@@ -42,7 +41,7 @@ class LingoTileSerializer(ArchesTileSerializer):
 
     @staticmethod
     def _check_pref_label_uniqueness(
-        data, resource, new_label_language, new_label_type
+        initial_tile_data, resource, new_label_language, new_label_type
     ):
         try:
             PREF_LABEL = ListItem.objects.get(list_item_values__value="prefLabel")
@@ -52,20 +51,22 @@ class LingoTileSerializer(ArchesTileSerializer):
             )
             raise ValidationError(msg)
 
-        for label in resource.appellative_status or []:
-            if label_languages := label.appellative_status_ascribed_name_language:
+        for label in resource.aliased_data.appellative_status:
+            if (
+                label_languages := label.aliased_data.appellative_status_ascribed_name_language
+            ):
                 label_language = label_languages[0]
             else:
                 continue
-            if label_types := label.appellative_status_ascribed_relation:
+            if label_types := label.aliased_data.appellative_status_ascribed_relation:
                 label_type = label_types[0]
             else:
                 continue
             if (
-                data.get("tileid") != label.tileid
+                initial_tile_data.get("tileid") != label.tileid
                 and new_label_type.uri == PREF_LABEL.uri
                 and label_type.uri == PREF_LABEL.uri
                 and label_language.uri == new_label_language.uri
             ):
                 msg = _("Only one preferred label per language is permitted.")
-                raise ValidationError(msg)
+                raise ValidationError({"appellative_status": msg})
