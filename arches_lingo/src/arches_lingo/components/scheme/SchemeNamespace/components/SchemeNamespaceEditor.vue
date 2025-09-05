fdd865100f44e3021823411411d7df93292a7e77
Christopher Byrd <chrabyrd@gmail.com>
2025-09-05 12:00:17 -0700

    Updates SchemeNamespace

--- arches_lingo/src/arches_lingo/components/scheme/SchemeNamespace/components/SchemeNamespaceEditor.vue
+++ arches_lingo/src/arches_lingo/components/scheme/SchemeNamespace/components/SchemeNamespaceEditor.vue
@@ -83,7 +83,9 @@ async function save(e: FormSubmitEvent) {
             const updatedScheme = await createLingoResource(
                 {
                     aliased_data: {
-                        [props.nodegroupAlias]: [updatedTileData],
+                        [props.nodegroupAlias]: {
+                            aliased_data: updatedTileData,
+                        },
                     },
                 },
                 props.graphSlug,
@@ -95,7 +97,7 @@ async function save(e: FormSubmitEvent) {
             });
 
             updatedTileId =
-                updatedScheme.aliased_data[props.nodegroupAlias][0].tileid;
+                updatedScheme.aliased_data[props.nodegroupAlias].tileid;
         } else {
             const updatedTile = await upsertLingoTile(
                 props.graphSlug,
