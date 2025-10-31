3349d7b61b35793151ea217911a3b5d397ad7a0e
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-10-30 20:06:20 -0400

    Reconfigure async lingo resource import for both RDM and SKOS sources #466

--- arches_lingo/tasks.py
+++ arches_lingo/tasks.py
@@ -8,21 +8,24 @@ from arches.app.tasks import notify_completion
 
 
 @shared_task
-def load_lingo_resources_task(**kwargs):
+def load_lingo_resources_task(loadid, userid, kwargs={}):
     logger = logging.getLogger(__name__)
+    status = _("Started")
 
     try:
-        importer = kwargs.get("importer")
+        importer = migrate_to_lingo.LingoResourceImporter(
+            loadid=loadid, userid=userid, **kwargs
+        )
         importer.run_load_task()
-        load_event = models.LoadEvent.objects.get(loadid=importer.loadid)
+        load_event = models.LoadEvent.objects.get(loadid=loadid)
         status = _("Completed") if load_event.status == "indexed" else _("Failed")
     except Exception as e:
         logger.error(e)
-        load_event = models.LoadEvent.objects.get(loadid=importer.loadid)
+        load_event = models.LoadEvent.objects.get(loadid=loadid)
         load_event.status = "failed"
         load_event.save()
         status = _("Failed")
     finally:
         msg = _("Lingo Resource Importer: [{}]").format(status)
-        user = User.objects.get(id=importer.userid)
+        user = User.objects.get(id=userid)
         notify_completion(msg, user)
