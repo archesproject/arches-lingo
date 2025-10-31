d9ed9c1150a0121436c70bab8afe9931c4c472bb
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-10-31 13:15:53 -0400

    Restabilize pkg import of thesauri #466

--- arches_lingo/management/commands/packages.py
+++ arches_lingo/management/commands/packages.py
@@ -1,6 +1,12 @@
+import uuid
+import os
+import io
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from arches.app.models import models
 from arches_controlled_lists.management.commands.packages import (
     Command as PackagesCommand,
 )
+from arches_lingo.etl_modules.migrate_to_lingo import LingoResourceImporter
 from arches_lingo.utils.skos import SKOSReader
 
 
@@ -19,6 +25,26 @@ class Command(PackagesCommand):
             self.import_lingo_resources(options["source"], options["overwrite"])
 
     def import_lingo_resources(self, source, overwrite_options):
-        skos = SKOSReader()
-        rdf = skos.read_file(source)
-        skos.orchestrate_bulk_import_from_skos(rdf, overwrite_options=overwrite_options)
+        file_name = os.path.basename(source)
+        with open(source, "rb") as f:
+            file_data = f.read()
+        inmemory_file = InMemoryUploadedFile(
+            file=io.BytesIO(file_data),
+            field_name="file",
+            name=file_name,
+            content_type="application/xml",
+            size=len(file_data),
+            charset=None,
+        )
+
+        self.loadid = str(uuid.uuid4())
+        bulk_loader = LingoResourceImporter(
+            loadid=self.loadid,
+            userid=models.User.objects.get(username="admin").pk,
+            mode="cli",
+        )
+        start_request = bulk_loader.start(request=None)
+        bulk_loader.file = inmemory_file
+        # Avoid using celery for package import
+        bulk_loader.config["celeryByteSizeLimit"] = 90000000  # 90mb
+        write_request = bulk_loader.write(request=None)
