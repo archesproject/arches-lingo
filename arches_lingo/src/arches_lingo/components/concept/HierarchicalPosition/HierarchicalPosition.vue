f56548e3ee8a83336a6afab270064e9c80aa7984
Christopher Byrd <chrabyrd@gmail.com>
2026-03-02 18:22:21 -0800

    prettier

--- arches_lingo/src/arches_lingo/components/concept/HierarchicalPosition/HierarchicalPosition.vue
+++ arches_lingo/src/arches_lingo/components/concept/HierarchicalPosition/HierarchicalPosition.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, watch} from "vue";
+import { onMounted, ref, watch } from "vue";
 
 import Message from "primevue/message";
 import Skeleton from "primevue/skeleton";
