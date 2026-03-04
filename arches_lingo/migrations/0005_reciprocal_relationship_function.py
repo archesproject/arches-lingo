3af2cf27c2694bdb56996a8977e059990cc5be83
Johnathan Clementi <johnathan.clementi1@gmail.com>
2026-03-04 09:59:26 -0500

    Revert "rm migration - function is registered during pkg load"
    This reverts commit 704a0a1ff5db65ec7e710a08656c0a36e20356a4.

---
+++ arches_lingo/migrations/0005_reciprocal_relationship_function.py
@@ -0,0 +1,38 @@
+from django.db import migrations
+
+
+FUNCTION_ID = "dc0a3a78-f8a3-4472-b77d-7301d6718a36"
+
+
+def forward(apps, schema_editor):
+    Function = apps.get_model("models", "Function")
+    Function.objects.update_or_create(
+        functionid=FUNCTION_ID,
+        defaults={
+            "modulename": "reciprocal_relationship.py",
+            "classname": "ReciprocalRelationshipFunction",
+            "functiontype": "node",
+            "name": "Reciprocal Relationship",
+            "description": "Ensures reciprocal relationship tiles exist between related Concepts",
+            "defaultconfig": {
+                "triggering_nodegroups": [],
+            },
+            "component": "",
+        },
+    )
+
+
+def reverse(apps, schema_editor):
+    Function = apps.get_model("models", "Function")
+    Function.objects.filter(functionid=FUNCTION_ID).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("arches_lingo", "0004_add_language_widget_mapping"),
+    ]
+
+    operations = [
+        migrations.RunPython(forward, reverse),
+    ]
