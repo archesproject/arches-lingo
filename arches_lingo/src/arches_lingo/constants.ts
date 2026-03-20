7b7049303305519619d425fb0ec92984c2de20bb
Rob Gaston <rgaston@fargeo.com>
2026-03-20 16:31:06 -0700

    removes dupe constants

--- arches_lingo/src/arches_lingo/constants.ts
+++ arches_lingo/src/arches_lingo/constants.ts
@@ -26,12 +26,6 @@ export const STRATEGY_DELETE_CHILDREN = "delete_children";
 export const STRATEGY_ORPHAN = "orphan";
 export const CONCEPT_TYPE_NODE_ALIAS = "type";
 
-export const DRAFT_LIFECYCLE_STATE_ID = "0e7f8c6d-1f7b-4c2a-9a0c-2b9e0d6c8f11";
-export const EDITING_LIFECYCLE_STATE_ID =
-    "b3a6a0d2-2b5c-4c2f-9d6c-0c2a5b7d1e8f";
-export const RETIRED_LIFECYCLE_STATE_ID =
-    "9d2e1c0b-7a6b-4b3d-8c1a-0f2d9e6b0a7c";
-
 export const LABEL_TYPE_LIST_ID = "deb847fc-f4c3-4e82-be19-04641579f129";
 export const NOTE_TYPE_LIST_ID = "aba2a0b4-75a4-45ba-8b57-021f3ca92a6a";
 export const CONCEPT_TYPE_LIST_ID = "4f9b2c82-59c6-4173-99ea-2a6bfbab6aa2";
@@ -56,7 +50,6 @@ export const GUIDE_TERM_ICON = "pi pi-angle-double-right";
 export const SCHEME_ICON = "pi pi-folder";
 export const TOP_CONCEPT_ICON = "pi pi-arrow-circle-up";
 
-// Lifecycle state IDs — fixed by migration 0009_update_scheme_concept_lifecycles
 export const DRAFT_LIFECYCLE_STATE_ID = "0e7f8c6d-1f7b-4c2a-9a0c-2b9e0d6c8f11";
 export const EDITING_LIFECYCLE_STATE_ID =
     "b3a6a0d2-2b5c-4c2f-9d6c-0c2a5b7d1e8f";
