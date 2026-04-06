6c8b0dadecdaaa092001d0c576e5c26beb8039e9
Johnathan Clementi <johnathan.clementi1@gmail.com>
2026-04-06 12:54:58 -0400

    Parameterize URI/Identifier import during skos import

--- arches_lingo/management/commands/packages.py
+++ arches_lingo/management/commands/packages.py
@@ -17,13 +17,33 @@ class Command(PackagesCommand):
         idx_of_operation_arg = [a.dest for a in parser._actions].index("operation")
         parser._actions[idx_of_operation_arg].choices.extend(["import_lingo_resources"])
 
+        parser.add_argument(
+            "--import-identifiers",
+            action="store_true",
+            default=False,
+            help="Import identifiers as URI/Identifier tiles and assign lifecycle states",
+        )
+        parser.add_argument(
+            "--namespace-template",
+            type=str,
+            default="",
+            help="Namespace URL template for the scheme (requires --import-identifiers)",
+        )
+
     def handle(self, *args, **options):
         super().handle(self, *args, **options)
 
         if options["operation"] == "import_lingo_resources":
-            self.import_lingo_resources(options["source"], options["overwrite"])
+            self.import_lingo_resources(
+                options["source"],
+                options["overwrite"],
+                import_identifiers=options["import_identifiers"],
+                namespace_template=options["namespace_template"],
+            )
 
-    def import_lingo_resources(self, source, overwrite_options):
+    def import_lingo_resources(
+        self, source, overwrite_options, import_identifiers=False, namespace_template=""
+    ):
         file_name = os.path.basename(source)
         with open(source, "rb") as f:
             file_data = f.read()
@@ -41,6 +61,8 @@ class Command(PackagesCommand):
             loadid=self.loadid,
             userid=models.User.objects.get(username="admin").pk,
             mode="cli",
+            import_identifiers=import_identifiers,
+            namespace_template=namespace_template,
         )
         start_request = bulk_loader.start(request=None)
         bulk_loader.file = inmemory_file
