79585153ee4a1bb65374ccf16081cfea982b794f
Rob Gaston <rgaston@fargeo.com>
2026-04-07 12:25:11 -0700

    adds saved sets to explore panel

--- arches_lingo/src/arches_lingo/components/header/PageHeader/components/SchemeHierarchy/SchemeHierarchy.vue
+++ arches_lingo/src/arches_lingo/components/header/PageHeader/components/SchemeHierarchy/SchemeHierarchy.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
 import { onMounted, provide, ref } from "vue";
+import { storeToRefs } from "pinia";
 
 import { useGettext } from "vue3-gettext";
 import { useToast } from "primevue/usetoast";
 
 import Button from "primevue/button";
+import Tab from "primevue/tab";
+import TabList from "primevue/tablist";
+import Tabs from "primevue/tabs";
 
 import ConceptTree from "@/arches_lingo/components/tree/ConceptTree.vue";
+import ConceptSetBrowser from "@/arches_lingo/components/concept-set-browser/ConceptSetBrowser.vue";
 
 import { fetchResourceInstanceLifecycleStates } from "@/arches_lingo/api.ts";
 
 import { ERROR, DEFAULT_ERROR_TOAST_LIFE } from "@/arches_lingo/constants.ts";
 
 import { useConceptStore } from "@/arches_lingo/stores/useConceptStore.ts";
+import { useUserStore } from "@/arches_lingo/stores/useUserStore.ts";
 
 const props = withDefaults(
     defineProps<{
@@ -27,9 +33,11 @@ const { $gettext } = useGettext();
 const toast = useToast();
 
 const conceptStore = useConceptStore();
+const { isAnonymous } = storeToRefs(useUserStore());
 
 const conceptTreeKey = ref(0);
 const resourceInstanceLifecycleStates = ref();
+const activeTabValue = ref("hierarchies");
 
 const emit = defineEmits<{
     (e: "shouldShowHierarchy", value: boolean): void;
@@ -58,12 +66,24 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="hierarchy-header-container">
-        <div class="hierarchy-header">
-            <h2 class="title">
-                {{ $gettext("Explore Hierarchies") }}
-            </h2>
-
+    <div class="scheme-hierarchy">
+        <div class="hierarchy-header-container">
+            <Tabs
+                v-model:value="activeTabValue"
+                class="hierarchy-tabs"
+            >
+                <TabList>
+                    <Tab value="hierarchies">
+                        {{ $gettext("Explore Hierarchies") }}
+                    </Tab>
+                    <Tab
+                        v-if="!isAnonymous"
+                        value="sets"
+                    >
+                        {{ $gettext("Concept Sets") }}
+                    </Tab>
+                </TabList>
+            </Tabs>
             <Button
                 icon="pi pi-times"
                 rounded
@@ -73,32 +93,65 @@ onMounted(async () => {
                 @click="emit('shouldShowHierarchy', false)"
             />
         </div>
+        <div class="hierarchy-content">
+            <ConceptTree
+                v-show="activeTabValue === 'hierarchies'"
+                :key="conceptTreeKey"
+                :is-open="props.isOpen"
+            />
+            <ConceptSetBrowser
+                v-if="!isAnonymous"
+                v-show="activeTabValue === 'sets'"
+            />
+        </div>
     </div>
-    <ConceptTree
-        :key="conceptTreeKey"
-        :is-open="props.isOpen"
-    />
 </template>
 
 <style scoped>
-.hierarchy-header-container {
-    background: var(--p-header-toolbar-background);
-}
-.hierarchy-header {
+.scheme-hierarchy {
+    height: 100%;
     display: flex;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-    padding-inline-start: 1rem;
-    padding-inline-end: 0.5rem;
+    flex-direction: column;
 }
 
-.title {
-    margin-top: 0px;
-    margin-bottom: 0px;
-    padding-top: 0.75rem;
-    padding-bottom: 0.75rem;
-    font-size: var(--p-lingo-font-size-large);
-    font-weight: var(--p-lingo-font-weight-normal);
+.hierarchy-header-container {
+    display: flex;
+    align-items: stretch;
+    background: var(--p-header-toolbar-background);
+    flex-shrink: 0;
+}
+
+.hierarchy-header-container :deep(.hierarchy-tabs) {
+    flex: 1;
+    min-width: 0;
+}
+
+.hierarchy-header-container :deep(.p-tablist) {
+    background: var(--p-header-toolbar-background);
+}
+
+.hierarchy-header-container :deep(.p-tablist-tab-list) {
+    border-color: var(--p-header-toolbar-border);
+}
+
+.hierarchy-header-container :deep(.p-tab) {
+    color: var(--p-header-item-label);
+}
+
+.hierarchy-header-container :deep(.p-tab-active) {
+    color: var(--p-primary-color);
+}
+
+.hierarchy-header-container
+    :deep(.p-tab:not(.p-tab-active):not(.p-disabled):hover) {
+    color: var(--p-text-color);
+}
+
+.hierarchy-content {
+    flex: 1 1 auto;
+    min-height: 0;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
 }
 </style>
