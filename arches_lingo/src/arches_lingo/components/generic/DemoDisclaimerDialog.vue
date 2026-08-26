d25984b8278db8625b32ea975c8b3e59c75b4456
Rob Gaston <rgaston@fargeo.com>
2026-08-26 14:32:35 -0700

    updates demo disclaimer text

--- arches_lingo/src/arches_lingo/components/generic/DemoDisclaimerDialog.vue
+++ arches_lingo/src/arches_lingo/components/generic/DemoDisclaimerDialog.vue
@@ -4,6 +4,7 @@ import { useGettext } from "vue3-gettext";
 import Button from "primevue/button";
 import Dialog from "primevue/dialog";
 
+import InlineMarkupText from "@/arches_lingo/components/generic/InlineMarkupText/InlineMarkupText.vue";
 import { useDemoDisclaimer } from "@/arches_lingo/composables/useDemoDisclaimer.ts";
 
 const { $gettext } = useGettext();
@@ -11,12 +12,21 @@ const { $gettext } = useGettext();
 const { isDemoDisclaimerVisible, acknowledgeDemoDisclaimer } =
     useDemoDisclaimer();
 
-// TODO: replace with the actual weekly refresh day before this is shared publicly.
-const siteRefreshDayOfWeek = "[DAY OF WEEK]";
-
-const editingAccessRequestMailto =
-    "mailto:contact@archesproject.org?subject=Lingo%20Demo%20Editing%20Access";
-const communityForumUrl = "https://community.archesproject.org/";
+const disclaimerParagraphs = [
+    $gettext(
+        "**This demo site is for evaluation purposes only.** The vocabularies loaded here should **NOT** be treated as an authoritative source.",
+    ),
+    $gettext(
+        "The *Getty Art and Architecture Thesaurus® (AAT)* and *General Multilingual Environmental Thesaurus (GEMET)* are locked (as of the dates they were loaded) and are not editable. They are included here only to demonstrate navigating, searching, and matching across vocabularies. The *Forum on Information Standards in Heritage (FISH)* has generously made their thesauri available for trial editing in a demo environment primarily to allow visitors to explore the flexibility and functionality of Lingo, including how vocabularies can be managed, modified, and extended.",
+    ),
+    $gettext(
+        "Should you make trial edits to the *FISH* vocabularies be aware that the **site is refreshed every week on Sunday**, reverting all edits and additions.",
+    ),
+    $gettext(
+        "To access editing functionality to *FISH*, please request an account by emailing us at [contact@archesproject.org](mailto:contact@archesproject.org?subject=Lingo%20Demo%20Editing%20Access) with the subject \u201cLingo Demo Editing Access.\u201d Questions or comments can be shared on the [Arches Community Forum](https://community.archesproject.org/) using the arches-lingo tag.",
+    ),
+    $gettext("For official vocabularies, see:"),
+];
 
 const officialVocabularyLinks = [
     {
@@ -44,54 +54,12 @@ const officialVocabularyLinks = [
         :style="{ width: '40rem', maxWidth: '90vw' }"
     >
         <div class="disclaimer-content">
-            <p class="dialog-text">
-                {{
-                    $gettext(
-                        "This demo site is for evaluation purposes only. The vocabularies loaded here should NOT be treated as an authoritative source.",
-                    )
-                }}
-            </p>
-
-            <p class="dialog-text">
-                {{
-                    $gettext(
-                        "The Getty Art and Architecture Thesaurus® (AAT) and General Multilingual Environmental Thesaurus (GEMET) are locked (as of the dates they were loaded) and are not editable. They are included here only to demonstrate navigating, searching, and matching across vocabularies. The Forum on Information Standards in Heritage (FISH) has generously made their thesauri available for trial editing in a demo environment primarily to allow visitors to explore the flexibility and functionality of Lingo, including how vocabularies can be managed, modified, and extended.",
-                    )
-                }}
-            </p>
-
-            <p class="dialog-text">
-                {{
-                    $gettext(
-                        "Should you make trial edits to the FISH vocabularies be aware that the site is refreshed every week on %{dayOfWeek}, reverting all edits and additions.",
-                        { dayOfWeek: siteRefreshDayOfWeek },
-                    )
-                }}
-            </p>
-
-            <p class="dialog-text">
-                {{
-                    $gettext(
-                        "To access editing functionality to FISH, please request an account by emailing us at",
-                    )
-                }}
-                <a :href="editingAccessRequestMailto"
-                    >contact@archesproject.org</a
-                >
-                {{ $gettext("with the subject “Lingo Demo Editing Access.”") }}
-                {{ $gettext("Questions or comments can be shared on the") }}
-                <a
-                    :href="communityForumUrl"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {{ $gettext("Arches Community Forum") }}
-                </a>
-                {{ $gettext("using the arches-lingo tag.") }}
-            </p>
-
-            <p class="dialog-text">
-                {{ $gettext("For official vocabularies, see:") }}
+            <p
+                v-for="disclaimerParagraph of disclaimerParagraphs"
+                :key="disclaimerParagraph"
+                class="dialog-text"
+            >
+                <InlineMarkupText :markup="disclaimerParagraph" />
             </p>
 
             <ul class="official-vocabulary-list">
