a56399b629362e425ab71f2f5be1c5aa049f8957
Aaron <124614+aarongundel@users.noreply.github.com>
2026-09-01 11:44:17 -0600

    updates post_load syntax (#795)
    * updates post_load syntax
    
    * adds vacuum/analyze
    
    ---------
    
    Co-authored-by: Rob Gaston <rgaston@fargeo.com>

--- .runtime/hooks/post_load.sh
+++ .runtime/hooks/post_load.sh
@@ -2,7 +2,8 @@
 /opt/ENV/bin/python manage.py load_lingo_fixtures --no-index
 
 # TEMPORARY - remove after 1.2.x release
-python manage.py dbshell -c "CREATE INDEX CONCURRENTLY IF NOT EXISTS tiles_uri_content_btree ON tiles ((tiledata -> 'bf73e64a-4888-11ee-8a8d-11afefc4bff7')) WHERE nodegroupid = 'bf73e598-4888-11ee-8a8d-11afefc4bff7';;"
-python manage.py dbshell -c "CREATE INDEX CONCURRENTLY IF NOT EXISTS tiles_part_of_scheme_covering ON tiles (resourceinstanceid) INCLUDE (tiledata, parenttileid, sortorder) WHERE nodegroupid = 'bf73e60a-4888-11ee-8a8d-11afefc4bff7';"
-python manage.py dbshell -c "CREATE INDEX CONCURRENTLY IF NOT EXISTS tiles_part_of_scheme_gin ON tiles USING GIN ((tiledata -> 'bf73e60a-4888-11ee-8a8d-11afefc4bff7') jsonb_path_ops) WHERE nodegroupid = 'bf73e60a-4888-11ee-8a8d-11afefc4bff7';"
-python manage.py dbshell -c "ANALYZE tiles;"
+echo "CREATE INDEX CONCURRENTLY IF NOT EXISTS tiles_uri_content_btree ON tiles ((tiledata -> 'bf73e64a-4888-11ee-8a8d-11afefc4bff7')) WHERE nodegroupid = 'bf73e598-4888-11ee-8a8d-11afefc4bff7';" | python manage.py dbshell
+echo "CREATE INDEX CONCURRENTLY IF NOT EXISTS tiles_part_of_scheme_covering ON tiles (resourceinstanceid) INCLUDE (tiledata, parenttileid, sortorder) WHERE nodegroupid = 'bf73e60a-4888-11ee-8a8d-11afefc4bff7';" | python manage.py dbshell
+echo "CREATE INDEX CONCURRENTLY IF NOT EXISTS tiles_part_of_scheme_gin ON tiles USING GIN ((tiledata -> 'bf73e60a-4888-11ee-8a8d-11afefc4bff7') jsonb_path_ops) WHERE nodegroupid = 'bf73e60a-4888-11ee-8a8d-11afefc4bff7';" | python manage.py dbshell
+echo "VACUUM ANALYZE tiles;" | python manage.py dbshell
+echo "VACUUM ANALYZE resource_instances;" | python manage.py dbshell
